bffc56079c79348a4bb76471752eaea7199ce395
송현서 <s22026@gsm.hs.kr>
2023-07-26 10:47:53 +0900

    기본세팅

--- app/src/main/java/com/seojunpark/android/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/seojunpark/android/presentation/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.seojunpark.android
+package com.seojunpark.android.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seojunpark.android.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
