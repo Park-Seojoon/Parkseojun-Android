5bc9c786a7715279290322687fec91e244371e8b
송현서 <s22026@gsm.hs.kr>
2023-07-26 11:40:10 +0900

    signup page 퍼블리싱

---
+++ app/src/main/java/com/seojunpark/android/presentation/ui/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.seojunpark.android.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seojunpark.android.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
